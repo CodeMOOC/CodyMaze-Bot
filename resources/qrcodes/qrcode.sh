7065e12992c237d3835b4f990fe6bbf45c956b4a
Saverio Delpriori <saverio.delpriori@uniurb.it>
2017-08-24 11:08:19 +0200

    Add QRCodes and related script

---
+++ resources/qrcodes/qrcode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+qr --factory=svg "http://t.me/codymazebot?start=a1" > a1.svg
+qr --factory=svg "http://t.me/codymazebot?start=a2" > a2.svg
+qr --factory=svg "http://t.me/codymazebot?start=a3" > a3.svg
+qr --factory=svg "http://t.me/codymazebot?start=a4" > a4.svg
+qr --factory=svg "http://t.me/codymazebot?start=a5" > a5.svg
+qr --factory=svg "http://t.me/codymazebot?start=b1" > b1.svg
+qr --factory=svg "http://t.me/codymazebot?start=b2" > b2.svg
+qr --factory=svg "http://t.me/codymazebot?start=b3" > b3.svg
+qr --factory=svg "http://t.me/codymazebot?start=b4" > b4.svg
+qr --factory=svg "http://t.me/codymazebot?start=b5" > b5.svg
+qr --factory=svg "http://t.me/codymazebot?start=c1" > c1.svg
+qr --factory=svg "http://t.me/codymazebot?start=c2" > c2.svg
+qr --factory=svg "http://t.me/codymazebot?start=c3" > c3.svg
+qr --factory=svg "http://t.me/codymazebot?start=c4" > c4.svg
+qr --factory=svg "http://t.me/codymazebot?start=c5" > c5.svg
+qr --factory=svg "http://t.me/codymazebot?start=d1" > d1.svg
+qr --factory=svg "http://t.me/codymazebot?start=d2" > d2.svg
+qr --factory=svg "http://t.me/codymazebot?start=d3" > d3.svg
+qr --factory=svg "http://t.me/codymazebot?start=d4" > d4.svg
+qr --factory=svg "http://t.me/codymazebot?start=d5" > d5.svg
+qr --factory=svg "http://t.me/codymazebot?start=e1" > e1.svg
+qr --factory=svg "http://t.me/codymazebot?start=e2" > e2.svg
+qr --factory=svg "http://t.me/codymazebot?start=e3" > e3.svg
+qr --factory=svg "http://t.me/codymazebot?start=e4" > e4.svg
+qr --factory=svg "http://t.me/codymazebot?start=e5" > e5.svg
+
+
+
+
