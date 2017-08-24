0f046afdc07ff9d6ff001619fb047e3e764a010c
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-08-24 10:32:17 +0200

    Maze commands base implementation

---
+++ src/maze_commands.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Nil command, does not execute any steps and returns the current coordinate.
+ */
+function command_nil($telegram_id, $current_coordinate) {
+    return array('', $current_coordinate);
+}
+
+function command_1($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_2($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_3($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_4($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_5($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_6($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_7($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_8($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_9($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_10($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_11($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_12($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
+
+function command_13($telegram_id, $current_coordinate) {
+    command_nil($telegram_id, $current_coordinate);
+}
