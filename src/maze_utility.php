1a2d4ae8b3b41df8fba23a75866d66ffbb5568a7
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-08-24 10:57:16 +0200

    Base implementation of maze utility functions

---
+++ src/maze_utility.php
@@ -0,0 +1,46 @@
+<?php
+/* UTILITY FUNCTIONS FOR QUERYING THE MAZE STRUCTURE */
+
+const DEFAULT_MAZE = array(
+    array(true , true , false, false, false),
+    array(false, false, false, false, true ),
+    array(false, false, false, false, false),
+    array(true , false, false, false, false),
+    array(false, false, true , false, false)
+);
+
+function coordinate_to_column($coordinate) {
+    if(strlen($coordinate) != 3) {
+        die('Coordinate with ' . strlen($coordinate) . ' values');
+    }
+
+    substr($coordinate, 0, 1)
+}
+
+function coordinate_to_row($coordinate) {
+
+}
+
+function coordinate_advance($coordinate) {
+
+}
+
+function coordinate_turn_left($coordinate) {
+
+}
+
+function coordinate_turn_right($coordinate) {
+
+}
+
+function coordinate_is_black($coordinate) {
+
+}
+
+function coordinate_empty_ahead($coordinate) {
+    return coordinate_empty_ahead($coordinate, 1);
+}
+
+function coordinate_empty_ahead($coordinate, $num) {
+
+}
