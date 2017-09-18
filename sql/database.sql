f98722f2ab91d36c303e439af034f7ffc80a3313
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-09-18 11:51:29 +0200

    Update database structure with log

--- sql/database.sql
+++ sql/database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Set 07, 2017 alle 08:59
--- Versione del server: 10.1.23-MariaDB-9+deb9u1
--- Versione PHP: 7.0.22-1~dotdeb+8.1
+-- Generation Time: Sep 18, 2017 at 09:50 AM
+-- Server version: 10.1.23-MariaDB-9+deb9u1
+-- PHP Version: 7.0.22-1~dotdeb+8.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `certificates_list`
+-- Table structure for table `certificates_list`
 --
 
 CREATE TABLE `certificates_list` (
@@ -32,19 +32,35 @@ CREATE TABLE `certificates_list` (
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `moves`
+-- Table structure for table `log`
 --
 
-CREATE TABLE `moves` (
-  `telegram_id` int(11) NOT NULL,
-  `reached_on` datetime DEFAULT NULL,
-  `cell` char(3) CHARACTER SET ascii NOT NULL
+CREATE TABLE `log` (
+  `log_id` int(10) UNSIGNED NOT NULL,
+  `severity` tinyint(3) NOT NULL,
+  `tag` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `message` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `telegram_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `user_status`
+-- Table structure for table `moves`
+--
+
+CREATE TABLE `moves` (
+  `telegram_id` int(11) NOT NULL,
+  `reached_on` datetime DEFAULT NULL,
+  `cell` char(3) CHARACTER SET ascii NOT NULL,
+  `last_callback_id` int(16) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_status`
 --
 
 CREATE TABLE `user_status` (
@@ -57,24 +73,40 @@ CREATE TABLE `user_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indici per le tabelle scaricate
+-- Indexes for dumped tables
 --
 
 --
--- Indici per le tabelle `certificates_list`
+-- Indexes for table `certificates_list`
 --
 ALTER TABLE `certificates_list`
   ADD PRIMARY KEY (`certificate_id`);
 
 --
--- Indici per le tabelle `moves`
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`log_id`),
+  ADD KEY `telegram_id` (`telegram_id`);
+
+--
+-- Indexes for table `moves`
 --
 ALTER TABLE `moves`
   ADD KEY `reached_on` (`reached_on`);
 
 --
--- Indici per le tabelle `user_status`
+-- Indexes for table `user_status`
 --
 ALTER TABLE `user_status`
   ADD PRIMARY KEY (`telegram_id`);
-COMMIT;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `log`
+--
+ALTER TABLE `log`
+  MODIFY `log_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
