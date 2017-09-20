abccd0b2257161ba5c8d6ab44836411a234f2022
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-09-20 11:17:42 +0200

    Fix command handling order

--- src/message_msg_processing.php
+++ src/message_msg_processing.php
@@ -14,25 +14,9 @@ function message_msg_processing($message) {
         // Get user info to see if he has reached end of game
         $user_info = db_row_query("SELECT * FROM user_status WHERE telegram_id = $chat_id LIMIT 1");
 
-        if (strpos($text, "/start") === 0) {
-            perform_command_start($chat_id, mb_strtolower($text));
-        }
-        else if ($text === "/reset"){
+        if ($text === "/reset"){
             reset_game($chat_id);
             telegram_send_message($chat_id, "Il tuo progresso è stato resettato.\nScrivi /start per ricomincare o scansiona un QRCode del CodyMaze.");
-        }
-        else if($text == "/send_certificates" && $chat_id == 212567799){
-            $result = db_table_query("SELECT * FROM user_status");
-            if($result !== null && $result !== false){
-                foreach ($result as $item) {
-                    $pdf_path = "certificates/" . $item[USER_STATUS_CERTIFICATE_ID] . ".pdf";
-                    $user_id = $item[0];
-                    if($item[USER_STATUS_COMPLETED] == 1 && $item[USER_STATUS_CERTIFICATE_SENT] == 0) {
-                        Logger::debug("Sending certificate to {$item[USER_STATUS_NAME]}");
-                        $result = telegram_send_document($user_id, $pdf_path, "Certificato di Completamento");
-                    }
-                }
-            }
         }
         else if (strpos($text, "/debug") === 0) {
             // Debugging commands received
@@ -40,6 +24,8 @@ function message_msg_processing($message) {
             debug_message_processing($chat_id, $text);
         }
         else if($user_info[USER_STATUS_COMPLETED] == 1) {
+            // Game is completed
+
             if (isset($memory->nameRequested)) {
                 // User is writing name for certificate
                 request_name($chat_id, $text);
@@ -48,12 +34,29 @@ function message_msg_processing($message) {
                 telegram_send_message($chat_id, "Hai completato CodyMaze!\nSe vuoi giocare nuovamente, invia il comando /reset.");
             }
         }
-        else if ($user_info[USER_STATUS_COMPLETED] == 0) {
-            // User is probably writing something instead of playing
-            telegram_send_message($chat_id, "Non ho capito. Scansionare un QRCode del labirinto per continuare a giocare.");
-        }
         else {
-            telegram_send_message($chat_id, "Non ho capito.");
+            // Game is not completed
+
+            if (strpos($text, "/start") === 0) {
+                perform_command_start($chat_id, mb_strtolower($text));
+            }
+            else if($text == "/send_certificates" && $chat_id == 212567799){
+                $result = db_table_query("SELECT * FROM user_status");
+                if($result !== null && $result !== false){
+                    foreach ($result as $item) {
+                        $pdf_path = "certificates/" . $item[USER_STATUS_CERTIFICATE_ID] . ".pdf";
+                        $user_id = $item[0];
+                        if($item[USER_STATUS_COMPLETED] == 1 && $item[USER_STATUS_CERTIFICATE_SENT] == 0) {
+                            Logger::debug("Sending certificate to {$item[USER_STATUS_NAME]}");
+                            $result = telegram_send_document($user_id, $pdf_path, "Certificato di Completamento");
+                        }
+                    }
+                }
+            }
+            else {
+                // User is probably writing something instead of playing
+                telegram_send_message($chat_id, "Non ho capito. Scansionare un QRCode del labirinto per continuare a giocare.");
+            }
         }
     }
     else {
