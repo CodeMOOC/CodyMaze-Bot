0b7cbed1a855e6fe63c030d27323894491bda5e3
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-02-03 18:18:57 +0100

    Bugfix in memory persistence

--- src/lib_memory.php
+++ src/lib_memory.php
@@ -33,18 +33,12 @@ function memory_persist($telegram_id) {
     global $memory;
 
     $encoded = json_encode($memory);
-    $update_result = db_perform_action("UPDATE `user_status` SET `memory` = '" . db_escape($encoded) . "', `last_memory_update` = NOW() WHERE `telegram_id` = {$telegram_id}");
+    $update_result = db_perform_action("INSERT INTO `user_status` (`telegram_id`, `last_memory_update`, `memory`) VALUES({$telegram_id}, NOW(), '" . db_escape($encoded) . "') ON DUPLICATE KEY UPDATE `last_memory_update` = NOW(), `memory` = '" . db_escape($encoded) . "'");
+
     if($update_result === false) {
-        Logger::warning("Failed to update conversation memory", __FILE__, $telegram_id);
-    }
-    else if($update_result === 0) {
-        // No user record, yet
-        if(db_perform_action("INSERT INTO `user_status` (`telegram_id`, `last_memory_update`, `memory`) VALUES({$telegram_id}, NOW(), '" . db_escape($encoded) . "')") === false) {
-            Logger::error("Failed to insert conversation memory", __FILE__, $telegram_id);
-        }
+        Logger::error("Failed to update conversation memory", __FILE__, $telegram_id);
     }
     else {
-        // All set
         Logger::debug("Conversation memory updated: {$encoded}", __FILE__, $telegram_id);
     }
 }
